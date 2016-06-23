3df08cec4a1f47f74b5f8592a5233570534b148c
Palmero, Antonio <antonio.palmero@here.com>
2016-06-23 15:00:22 +0200

    Initial commit

---
+++ ACPReminderLib/ACPReminder.swift
@@ -0,0 +1,28 @@
+//
+//  ACPReminder.swift
+//  ACPReminder
+//
+//  Created by Palmero, Antonio on 23/06/16.
+//  Copyright © 2016 Uttopia. All rights reserved.
+//
+
+import UIKit
+
+class ACPReminder {
+    var randomMessage : Boolean = false
+    var circularTimePeriod : Boolean = false
+    var appDomain : String = ""
+    var messages : [String] = []
+    var timePeriods : [Int] = []
+    var testFlagSeconds : Boolean = false
+    
+    static let sharedInstance = ACPReminder()
+    
+    private init() {
+        
+    }
+
+    
+    
+    
+}
---
+++ ACPReminderLib/ACPReminderLib.h
@@ -0,0 +1,19 @@
+//
+//  ACPReminderLib.h
+//  ACPReminderLib
+//
+//  Created by Palmero, Antonio on 23/06/16.
+//  Copyright © 2016 Uttopia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ACPReminderLib.
+FOUNDATION_EXPORT double ACPReminderLibVersionNumber;
+
+//! Project version string for ACPReminderLib.
+FOUNDATION_EXPORT const unsigned char ACPReminderLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ACPReminderLib/PublicHeader.h>
+
+
